9a1f501d218b00e7771c1767b7c85871074e74ec
EL JARRAÏ <eljarrai.reda@gmail.com>
2021-03-19 19:49:17 +0100

    refactoring compute function

--- fizzbuzzproject/src/main/java/org/example/FizzBuzz.java
+++ fizzbuzzproject/src/main/java/org/example/FizzBuzz.java
@@ -8,12 +8,6 @@ public class FizzBuzz
 {
     public static String compute(int number)
     {
-        if (number == 2){
-            return "2";
-        }
-        if (number == 4){
-            return "4";
-        }
-       return "1";
+        return String.valueOf(number);
     }
 }
